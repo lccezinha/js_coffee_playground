4b00a05d6d91154ce4ad30b67ff0481451f2fdc2
Luiz Cezer <lccezinha@gmail.com>
2014-05-29 20:51:34 -0300

    comentários

--- js/playing/scopes.js
+++ js/playing/scopes.js
@@ -1,5 +1,8 @@
 // compila a Regex quando o arquivo é carregado, ou seja, uma única vez.
-// permance constante durante a execução
+// permance constante durante a execução. (melhor performance)
 var re = /ab+c/;
 
+// compila em tempo de execução
+// recomendado usar quando se sabe que a regra da RegExp irá mudar constantemente
+// ou quando não se sabe o padrão da mascará
 var re = RegExp("ab+c");
