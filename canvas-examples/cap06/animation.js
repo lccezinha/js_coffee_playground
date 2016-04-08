eded8e40a4b994153a62b65fa6443ad4b9b69009
Luiz Cezer <lccezinha@gmail.com>
2016-04-07 21:28:51 -0300

    Background do jogo com animação

---
+++ canvas-examples/cap06/animation.js
@@ -0,0 +1,37 @@
+function Animation(context) {
+   this.context = context;
+   this.sprites = [];
+   this.online = false;
+}
+Animation.prototype = {
+   newSprite: function(sprite) {
+      this.sprites.push(sprite);
+   },
+   start: function() {
+      this.online = true;
+      this.nextFrame();
+   },
+   off: function() {
+      this.online = false;
+   },
+   nextFrame: function() {
+      if ( ! this.online ) return;
+
+      this.clearScreen();
+
+      for (var i in this.sprites)
+         this.sprites[i].update();
+
+      for (var i in this.sprites)
+         this.sprites[i].draw();
+
+      var animation = this;
+      requestAnimationFrame(function() {
+         animation.nextFrame();
+      });
+   },
+   clearScreen: function() {
+      var ctx = this.context;
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+   }
+}
