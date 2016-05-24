1322be0e3fe3731c511020a71626b5e6b31acfcf
Luiz Cezer <lccezinha@gmail.com>
2016-05-24 20:51:38 -0300

    Exibindo painel com qtd de pontos

--- canvas-examples/cap08/panel.js
+++ canvas-examples/cap08/panel.js
@@ -4,6 +4,7 @@ function Panel(context, plane) {
   this.spritesheet = new Spritesheet(context, plane.image, 3, 2);
   this.spritesheet.line = 0;
   this.spritesheet.column = 0;
+  this.score = 0;
 }
 
 Panel.prototype = {
@@ -22,5 +23,12 @@ Panel.prototype = {
     }
 
     this.context.scale(2, 2);
+
+    var ctx = this.context;
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.font = '18px sans-serif';
+    ctx.fillText(this.score, 100, 28);
+    ctx.restore();
   }
 }
