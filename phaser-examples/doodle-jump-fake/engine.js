5a39ff0ff9d6d94111e09f922a8cc8df6d5aceca
Luiz Cezer <lccezinha@gmail.com>
2016-06-03 23:04:06 -0300

    Doodle Jump Fake: adição de primeiros obstáculos

--- phaser-examples/doodle-jump-fake/engine.js
+++ phaser-examples/doodle-jump-fake/engine.js
@@ -1,14 +1,30 @@
 var game = new Phaser.Game(400, 500);
+var platforms;
 
 var mainState = {
   preload: function() {
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
+    game.load.image('brick', 'assets/brick.png')
   },
 
   create: function() {
     game.add.sprite(0, 0, 'sky');
-    game.add.sprite(0, game.world.height - 30, 'ground');
+
+    platforms = game.add.group();
+    platforms.enableBody = true;
+
+    var ground = platforms.create(0, game.world.height - 30, 'ground');
+    ground.body.immovable = true;
+
+    var ledge = platforms.create(-200, 50, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-250, 300, 'ground');
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(250, 200, 'ground');
+    ledge.body.immovable = true;
   },
 
   update: function() {
